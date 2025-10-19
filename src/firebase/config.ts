e5a20ddc1d2f5e3a9182074560b97e7874b0fffc
Game Make <makegame227@gmail.com>
2025-10-19 21:09:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-148302829-97483",
+  "appId": "1:345015058787:web:fd33abb8683a86a91f1356",
+  "apiKey": "AIzaSyAR67qM6zqkYYAEa14zPQQOGNwqnnY4XXY",
+  "authDomain": "studio-148302829-97483.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "345015058787"
+};
